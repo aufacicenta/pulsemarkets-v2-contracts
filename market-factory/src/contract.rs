4dcce00bc02c5e66fd7dfba6319129d637e80221
svpervnder <svpervnder@MacBook-Pro.local>
2022-05-04 11:41:30 -0600

    feat: MarketFactory process_market_options promise

--- market-factory/src/contract.rs
+++ market-factory/src/contract.rs
@@ -26,33 +26,49 @@ impl MarketFactory {
     }
 
     #[payable]
-    pub fn create_market(&mut self, description: String, market_options: Vec<String>) -> Promise {
+    pub fn create_market(
+        &mut self,
+        description: String,
+        dao_account_id: AccountId,
+        market_options: Vec<String>,
+    ) -> Promise {
         let name = format!("market_{}", self.markets.len() + 1);
         let market_account_id: AccountId = format!("{}.{}", name, env::current_account_id())
             .parse()
             .unwrap();
 
-        let promise = Promise::new(market_account_id.clone())
+        let create_market_promise = Promise::new(market_account_id.clone())
             .create_account()
             .add_full_access_key(env::signer_account_pk())
             .transfer(env::attached_deposit())
             .deploy_contract(MARKET_CODE.to_vec())
             .function_call(
                 "new".to_string(),
-                json!({ "description": description, "market_options": market_options })
+                json!({ "description": description, "dao_account_id": dao_account_id })
                     .to_string()
                     .into_bytes(),
                 0,
                 GAS_FOR_CREATE_MARKET,
             );
 
-        let callback = Promise::new(env::current_account_id()).function_call(
+        let process_market_options_promise = Promise::new(market_account_id.clone()).function_call(
+            "process_market_options".to_string(),
+            json!({ "market_options": market_options })
+                .to_string()
+                .into_bytes(),
+            0,
+            GAS_FOR_PROCESS_MARKET_OPTIONS,
+        );
+
+        let create_market_callback = Promise::new(env::current_account_id()).function_call(
             "on_create_market_callback".to_string(),
             json!({}).to_string().into_bytes(),
             0,
             GAS_FOR_CREATE_MARKET_CALLBACK,
         );
 
-        promise.then(callback)
+        create_market_promise
+            .and(process_market_options_promise)
+            .then(create_market_callback)
     }
 }
