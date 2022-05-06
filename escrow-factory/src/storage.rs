caeac5460f6d5686ef3ab84580a8320dbd8a6ebb
susguzman <susguzman@gmail.com>
2022-05-05 22:31:42 -0600

    chore: remove escrow contracts

--- escrow-factory/src/storage.rs
+++
@@ -1,9 +0,0 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::UnorderedSet;
-use near_sdk::{near_bindgen, AccountId};
-
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct EscrowFactory {
-    pub conditional_escrow_contracts: UnorderedSet<AccountId>,
-}
