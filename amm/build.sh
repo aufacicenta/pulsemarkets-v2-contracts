e4bb7419fa244e436346695eb50cf4de334f1054
Gustavo Ibarra <gus@aufacicenta.com>
2022-06-04 13:07:38 -0600

    chore(AMM): build.sh directory

--- amm/build.sh
+++ amm/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp ./target/wasm32-unknown-unknown/release/amm.wasm res/
+cp ./target/wasm32-unknown-unknown/release/amm.wasm build/
