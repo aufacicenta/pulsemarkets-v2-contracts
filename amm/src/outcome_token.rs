47a2b82292beb73dfadb69e2e269ea82276b4bfc
Gustavo Ibarra <gus@aufacicenta.com>
2023-01-31 15:29:47 -0600

    chore: removes unnecessary outcome_token methods

--- amm/src/outcome_token.rs
+++ amm/src/outcome_token.rs
@@ -123,36 +123,3 @@ impl OutcomeToken {
         self.is_active
     }
 }
-
-impl OutcomeToken {
-    /**
-     * @notice deposit tokens into an account
-     * @param account_id the account_id to deposit into
-     * @param amount the amount of tokens to deposit
-     */
-    fn _deposit(&mut self, receiver_id: &AccountId, amount: WrappedBalance) {
-        assert_eq!(self.is_active, true, "ERR_DEPOSIT_INACTIVE");
-        assert!(amount > 0, "ERR_DEPOSIT_AMOUNT_LOWER_THAN_0");
-
-        let receiver_balance = self.balances.get(&receiver_id).unwrap_or(0);
-        let new_balance = receiver_balance + amount;
-
-        self.balances.insert(&receiver_id, &new_balance);
-    }
-
-    /**
-     * @notice withdraw token from an account
-     * @param account_id to withdraw from
-     * @param amount of tokens to withdraw
-     */
-    fn _withdraw(&mut self, sender_id: &AccountId, amount: WrappedBalance) {
-        assert_eq!(self.is_active, true, "ERR_WITHDRAW_INACTIVE");
-        let sender_balance = self.balances.get(&sender_id).unwrap_or(0);
-
-        assert!(amount > 0, "ERR_WITHDRAW_AMOUNT_LOWER_THAN_0");
-        assert!(sender_balance >= amount, "ERR_BALANCE_LOWER_THAN_AMOUNT");
-
-        let new_balance = sender_balance - amount;
-        self.balances.insert(&sender_id, &new_balance);
-    }
-}
